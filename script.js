077c442313d0620d1cd23d7adeb66ec4b095ee58
er-lau <118472365+er-lau@users.noreply.github.com>
2024-05-15 15:15:09 +0200

    Add function playRound

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+let humanScore = 0;
+let computerScore = 0;
+
+
+
+
+
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3);
     switch (randomNumber) {
@@ -13,7 +20,39 @@ function getComputerChoice() {
 function getHumanChoice() {
     let humanChoice;
     while (humanChoice !== "rock" && humanChoice !== "paper" && humanChoice !== "scissors") {
-        humanChoice = prompt("Enter rock, paper or scissors.");
+        humanChoice = prompt("Enter rock, paper or scissors.").toLowerCase();
     }
     return humanChoice;
+}
+
+function playRound(humanChoice, computerChoice) {
+    switch (humanChoice) {
+        
+        case humanChoice === "rock" && computerChoice === "scissors":
+            console.log(`You win! Rock beats Scissors.`);
+            humanScore++;
+            break;
+        case humanChoice === "rock" && computerChoice === "paper":
+            console.log(`You lose! Paper beats Rock.`);
+            computerScore++;
+            break;
+        case humanChoice === "scissors" && computerChoice === "rock":
+            console.log(`You lose! Rock beats Scissors.`);
+            computerScore++;
+            break;
+        case humanChoice === "scissors" && computerChoice === "paper":
+            console.log(`You win! Scissors beat Paper.`);
+            humanScore++;
+            break;
+        case humanChoice === "paper" && computerChoice === "scissors":
+            console.log(`You lose! Scissors beat Paper.`);
+            computerScore++;
+            break;
+        case humanChoice === "paper" && computerChoice === "rock":
+            console.log(`You win! Paper beats Rock.`);
+            humanScore++;
+            break;
+        default:
+            console.log(`It's a draw! You both chose ${humanChoice}.`);
+    }
 }
